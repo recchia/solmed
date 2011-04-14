f1a270c1e3d569a68e67c6b0c0dc218e7fbf1ef9
Piero <piero.recchia@gmail.com>
2011-04-14 19:17:59 -0430

    Actualización de password terminada

--- apps/frontend/lib/myUser.class.php
+++ apps/frontend/lib/myUser.class.php
@@ -2,4 +2,17 @@
 
 class myUser extends sfGuardSecurityUser
 {
+    public function cambiarPassword($user, $password, $new_pass, $confirm) {
+        if($user->checkPassword($password)) {
+            if($new_pass == $confirm) {
+                $user->setPassword($new_pass);
+                $user->save();
+                return array('true','Su contraseña ha sido cambiada con éxito');
+            } else {
+                return array('false','Las contraseñas no coinciden');
+            }
+        } else {
+            return array('false','Contraseña actual incorrecta');
+        }
+    }
 }

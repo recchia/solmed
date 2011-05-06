bac6b181241c07f5614546314806160d222bcd06
Piero <piero.recchia@gmail.com>
2011-05-06 16:01:17 -0430

    Modificada lista de inventario

--- apps/frontend/modules/inventario/templates/indexSuccess.php
+++ apps/frontend/modules/inventario/templates/indexSuccess.php
@@ -11,12 +11,10 @@
   <tbody>
     <?php foreach ($inventarios as $inventario): ?>
     <tr>
-      <td><a href="<?php echo url_for('inventario/edit?id='.$inventario->getId()) ?>"><?php echo $inventario->getArticulo() ?></a></td>
+      <td><?php echo $inventario->getArticulo() ?></td>
       <td><?php echo $inventario->getCantidad() ?></td>
-      <td><?php echo $inventario->getFechaVencimiento() ?></td>
+      <td><?php echo date('d-m-Y', strtotime($inventario->getFechaVencimiento())) ?></td>
     </tr>
     <?php endforeach; ?>
   </tbody>
 </table>
-<br>
-<a href="<?php echo url_for('inventario/new') ?>" id="boton_link" class="ui-state-default ui-corner-all"><span class="ui-icon ui-icon-plus"></span>Cargar Nuevo Inventario</a>
